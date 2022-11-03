914e0fc63571285043d3403fe265398aba6bc56b
ZeckeyBay <zfatihturan@gmail.com>
2022-11-03 14:15:53 +0300

    app is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, {useState} from "react";
 import UserList from './Components/UserList';
+import UserDetail from './Components/UserDetail';
 import './App.css';
 
 function App() {
+  const [activeUserId,setActiveUserId] = useState(null);
   return (
     <div className="App">
-      <UserList></UserList>
-      <div>sağ</div>
+      Active User Id : {activeUserId}
+      <UserList setActiveUserId={setActiveUserId} />
+      {activeUserId&&<UserDetail activeUserId={activeUserId}/>}
     </div>
   );
 }
